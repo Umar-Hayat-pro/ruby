1c2edea6364aedb9e3ac18476b39a845420e3eb3
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-20 12:30:10 +0500

    Array methods learned

---
+++ arraymethod.rb
@@ -0,0 +1,15 @@
+array_1 = ['Html','css','javascript','Bootstrap']
+
+array_1.push('Ruby')
+# puts array_1
+# puts array_1.pop
+# print array_1
+# puts array_1.reverse
+puts array_1.sort
+
+puts array_1.include?"Python"
+puts array_1.include?"Ruby"
+
+puts array_1
+
+print array_1
