9328462bfac302148d5dc3d312c002d4caba1b54
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-20 12:12:17 +0500

    Learned about arrays inside an array  Insider array

---
+++ grid.rb
@@ -0,0 +1,14 @@
+# this array is like objects in javascript having arrays inside an array
+
+grid = [[1,2,3],[4,5,6],[7,8,9]]
+
+puts grid[0][1] # 2
+puts grid[1][0] # 4
+
+puts grid[1][3] = 10
+
+puts grid[1][3]
+
+puts grid
+
+print grid
