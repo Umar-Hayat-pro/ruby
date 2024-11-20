d39df3278c64bb3763ca7d87bd1d960142a03e63
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-20 13:55:12 +0500

    Learned about conditons and made the first assignment project

---
+++ Greeting based on time of day/program.rb
@@ -0,0 +1,20 @@
+puts "Enter the Current Time in 24hrs Format"
+time = gets.chomp
+
+# Input Validation
+if time =~ /\D/ || time.to_i < 0 || time.to_i > 23
+  puts "Invalid Input"
+else
+  time = time.to_i # Convert to integer for further comparisons
+
+  # Greeting Logic
+  if time >= 5 && time <= 11
+    puts "Good Morning"
+  elsif time >= 12 && time <= 17
+    puts "Good Afternoon"
+  elsif time >= 18 && time <= 22
+    puts "Good Evening"
+  else
+    puts "Good Night"
+  end
+end
---
+++ condition.rb
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+if a > b
+  puts "a is greater than b"  
+else
+  puts "b is greater than a"
+end
+
+
+
+unless a > b
+ puts "b is greater than a" 
+else
+  puts "a is  greater than b"
+end
