fdd325bf6c2ba1d569094aa02f64990a0f3d06ac
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-21 15:24:47 +0500

    simple calculator done

---
+++ calculator/program.rb
@@ -0,0 +1,21 @@
+puts "Input a Number:"
+input_i = gets.chomp.to_f
+
+puts "Input An Operator such as (+,-,x,/)"
+operator = gets.chomp
+
+puts "Input another number:"
+input_ii = gets.chomp.to_i
+
+answer = case operator
+when '+'
+    puts input_i + input_ii
+when '-'
+    puts input_i - input_ii
+when '*'
+    answer = input_i * input_ii 
+    puts answer
+when '/'
+    answer = input_i / input_ii
+    puts answer.to_i
+end
