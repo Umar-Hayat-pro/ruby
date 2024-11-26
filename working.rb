2e8b100874f4426a90cbeb4d240cce67b0aadf45
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-26 09:47:05 +0500

    Learning Hashes

---
+++ working.rb
@@ -0,0 +1,14 @@
+# puts "Enter a number"
+# num1 = gets.chomp.to_i
+# puts "ENter another number"
+# num2 = gets.chomp.to_i
+
+# sum = num1 + num2   
+# puts "Sum is: #{sum}"
+
+
+#madlibz
+
+puts ("Roses are {color}")
+puts ("{plural_noun} are blue")
+puts ("I love {celebrity}")
