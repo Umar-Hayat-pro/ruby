15c3f539b6e2d5096624ac5f9e1dd9d0aff3b065
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2024-11-26 11:47:07 +0500

    COntinue learning Ruby

--- working.rb
+++ working.rb
@@ -9,6 +9,31 @@
 
 #madlibz
 
-puts ("Roses are {color}")
-puts ("{plural_noun} are blue")
-puts ("I love {celebrity}")
+# puts ("Roses are {color}")
+# puts ("{plural_noun} are blue")
+# puts ("I love {celebrity}")
+
+
+# def sayhi(name='NO Name',age=-1)
+#     puts ('Hello ' + name + ' You are ' +  age.to_s + ' years old')
+# end
+
+# sayhi('Umar')
+
+
+# def cube(num)
+#    return num * num * num 
+# end
+
+# puts cube(5)
+
+ismale = false
+istall = false
+ 
+if ismale and istall 
+  puts "You are a tall male"
+elsif ismale and !istall 
+    puts "You are a male but short in height DWarf"
+else
+    puts "You are not a  male."
+end
